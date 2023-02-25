3c7dbbf0a910d64744c6996935c5f11e20e24377
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-02-25 14:45:57 +0600

    templete-string

--- arrow.js
+++ arrow.js
@@ -1,3 +1,9 @@
-const multiply = (a, b , c) => a*b*c;
-const result = multiply(2, 3, 4);
-console.log(result);
+// const multiply = (a, b , c) => a*b*c;
+// const result = multiply(2, 3, 4);
+// console.log(result);
+
+const multiply = (a, b, c) =>{
+    return result= a*b*c;
+}
+const total = multiply(2,2,3);
+console.log(total);
---
+++ templete-string.js
@@ -0,0 +1,4 @@
+const multiline = `I am a web developer.
+I love to code.
+I love to eat biryani.`;
+console.log(multiline);
